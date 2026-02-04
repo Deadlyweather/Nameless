1283f3b3bcf8e1a372fcc1f1f3b11d6bf74c56a5
Tony Ruotsalainen <tonyruotsalainenuusi@gmail.com>
2026-02-04 13:35:59 +0200

    test

---
+++ koodi.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById("screen");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let scores = {
+    floor: 0,
+    score: 0
+};
+
+let blueprints = {
+    codes: {
+        wall: "black",
+        floor: "grey"
+    },
+    bottom: "..Rakenteet/Blueprints/Floor0.png"
+}
+
+let images = {
+    wall: "..Rakenteet/Walls/Wall0.png",
+    floor: "..Rakenteet/Floors/Floor0.png"
+}
+
+let building = {
+    
+}
+
+function scoreboard() {
+    ctx.fillStyle = "grey";
+    ctx.fillRect(50, 50, 200, 120);
+
+    ctx.fillStyle = "black";
+    ctx.font = "40px Arial";
+    ctx.fillText("Score: " + scores.score, 50, 100);
+    ctx.fillText("Floor: " + scores.floor, 50, 150);
+}
+
+function build(Blueprint) {
+    
+}
+
+function floor() {
+    if (scores.floor === 0) {
+        build(blueprints.bottom);
+    }
+}
+
+function gameloop() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    scoreboard();
+
+    requestAnimationFrame(gameloop);
+}
+
+gameloop();
